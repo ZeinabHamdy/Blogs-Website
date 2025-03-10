2299bf30e623aaeff98a08ec82c85b8078fadeb5
ZeinabHamdy <zeinabhamdy1010@gmail.com>
2025-03-11 01:36:59 +0200

    enhanced registeration and navbar

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register_view(req):
-    form = UserCreationForm()
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(req, f'Account created for {form.cleaned_data['username']} successfully')
+            return redirect('home')
+    else :
+        form = UserCreationForm()
     context = {
         'form': form,
     }
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='home'),
     path('post/<int:pk>/', views.post_details, name='post_details'),
     path('post_share/<int:pk>/', views.post_share, name='post_share'),
 ]
