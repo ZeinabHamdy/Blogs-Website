577cb84bad8329e8e1b43d6be300a4e98f91f4a9
ZeinabHamdy <zeinabhamdy1010@gmail.com>
2025-03-11 01:46:46 +0200

    delete db

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+
+
 def register_view(req):
     if req.method == 'POST':
         form = UserCreationForm(req.POST)
